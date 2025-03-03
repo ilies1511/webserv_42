78b01202c96452c60391fc2ac59ea2d38c0a62ab
ilies1511 <iziane@student.42heilbronn.de>
2025-03-03 14:04:43 +0100

    FEAT(pollserver): Add Buffer Wrapper --> instead of using raw std::vector<char> _buffer

---
+++ Includes/utils/Buffer.hpp
@@ -0,0 +1,34 @@
+#ifndef Buffer_HPP
+#define Buffer_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Buffer
+{
+	private:
+		static constexpr size_t INITIAL_SIZE = 4096;
+	public:
+		std::vector<char> _buffer;
+		Buffer(void);
+		~Buffer(void);
+	private:
+	//OCF -- BEGIN
+		Buffer(const Buffer& other) = delete;
+		Buffer& operator=(const Buffer& other) = delete;
+	//OCF -- END
+
+	//Methodes -- BEGIN
+	public:
+		// ssize_t	read(int fd);
+		// ssize_t	write(int fd) const;
+		// bool	contains_crlf() const;
+		// void	consume(size_t n);
+
+		const char*	data() const;
+		size_t size() const;
+	//Methodes -- ENDs
+};
+
+#endif
